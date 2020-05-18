ede6e2f9c6fa91aec6de55ec03940af13289adc8
Sanjeev Yogi <yogi.sjv@gmail.com>
2020-05-18 21:02:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  new_array = array.push(element)
+  new_array
 end
 
 def add_element_to_start_of_array(array, element)
