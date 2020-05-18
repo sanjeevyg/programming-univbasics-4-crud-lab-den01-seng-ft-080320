a92a9567487118d8a65c675522b9cd7cdcf07edb
Sanjeev Yogi <yogi.sjv@gmail.com>
2020-05-18 21:03:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  new_array = array.unshift(element)
+  new_array
 end
 
 def remove_element_from_end_of_array(array)
